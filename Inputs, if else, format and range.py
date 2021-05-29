8ea4703c507637e25821fc459fde2f1fb2a6ff56
moigr <checkmyleetskill@gmail.com>
2021-05-29 19:08:41 +0300

    Dictionaries and sets homework

--- Inputs, if else, format and range.py
+++ Inputs, if else, format and range.py
@@ -1,20 +1,20 @@
-# for i in range(1,13):
-#    print ("Numarul {} patrat e {} si cub e {:4}".format(i,i**2,i**3));
-
-name = input(" Numele: ")
-age = int(input(" Ce varsta ai, {0}?".format(name)))
-print(age)
-
-#if age >= 18:
-#    print("E ok, poti vota")
-#    print(" Pune un x in cutie")
+# # for i in range(1,13):
+# #    print ("Numarul {} patrat e {} si cub e {:4}".format(i,i**2,i**3));
+#
+# name = input(" Numele: ")
+# age = int(input(" Ce varsta ai, {0}?".format(name)))
+# print(age)
+#
+# #if age >= 18:
+# #    print("E ok, poti vota")
+# #    print(" Pune un x in cutie")
+# # else:
+# #    print("Nu ai ce cota aici.Hai inapoi peste {0} ani".format(18 - age))
+#
+# if age < 18:
+#     print("Nu ai ce cota aici.Hai inapoi peste {0} ani".format(18 - age))
+# elif age == 900:
+#     print("Wasted")
 # else:
-#    print("Nu ai ce cota aici.Hai inapoi peste {0} ani".format(18 - age))
-
-if age < 18:
-    print("Nu ai ce cota aici.Hai inapoi peste {0} ani".format(18 - age))
-elif age == 900:
-    print("Wasted")
-else:
-    print("poti vota")
-    print("Da-i sa mearga cu voturileeee")
+#     print("poti vota")
+#     print("Da-i sa mearga cu voturileeee")
---
+++ course_4/main.py
@@ -0,0 +1,6 @@
+def id_type_value():
+    print("course 4")
+    pass
+
+if __name__ == '__main__':
+    id_type_value()
